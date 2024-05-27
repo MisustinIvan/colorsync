7091cb568ba5f1452cfa2abfb6dbb14c2d9ba47e
Ivan Misustin <ivanmis596@gmail.com>
2024-05-27 15:47:58 +0200

    test

--- lua/colorsync.lua
+++ lua/colorsync.lua
@@ -29,7 +29,7 @@ local picker = function(array, callback)
 	local actions = require("telescope.actions")
 	local action_state = require("telescope.actions.state")
 
-	local opts = {}
+	local opts = conf.themes.get_dropdown()
 
 	return pickers
 		.new(opts, {
